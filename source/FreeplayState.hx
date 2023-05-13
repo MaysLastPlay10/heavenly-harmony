a49bb9ee0bf73a47c0124958f2cd365836d80062
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-05-13 16:40:47 +0300

    update source/FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -275,7 +275,11 @@ class FreeplayState extends MusicBeatState
 		funkay.scrollFactor.set();
 		funkay.screenCenter();
 		funkay.alpha = 0;
-		
+
+   #if mobile
+   addVirtualPad(UP_DOWN, A_B_C_X_Y_Z);
+   #end
+	
 		super.create();
 	}
 
@@ -344,8 +348,8 @@ class FreeplayState extends MusicBeatState
 		var upP = controls.UI_UP_P;
 		var downP = controls.UI_DOWN_P;
 		var accepted = controls.ACCEPT;
-		var space = FlxG.keys.justPressed.SPACE;
-		var ctrl = FlxG.keys.justPressed.CONTROL;
+		var space = FlxG.keys.justPressed.SPACE #if android || virtualPad.buttonX.justPressed #end;
+		var ctrl = FlxG.keys.justPressed.CONTROL #if android || virtualPad.buttonC.justPressed #end;
 
 		var shiftMult:Int = 1;
 		if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
@@ -481,7 +485,7 @@ class FreeplayState extends MusicBeatState
 			}
 			});
 		}
-		else if(controls.RESET)
+		else if(controls.RESET #if mobile || virtualPad.buttonY.justPressed #end)
 		{
 			persistentUpdate = false;
 			openSubState(new ResetScoreSubState(songs[curSelected].songName, curDifficulty, songs[curSelected].songCharacter));
