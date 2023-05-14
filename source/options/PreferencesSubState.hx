e65ee58c08c0aeb2d04273bf92221a38f6f6a296
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-05-14 11:49:02 +0300

    update source/options/PreferencesSubState.hx

--- source/options/PreferencesSubState.hx
+++ source/options/PreferencesSubState.hx
@@ -177,7 +177,7 @@ class PreferencesSubState extends BaseOptionsMenu
 			'Check this if you want to play with\na controller instead of using your Keyboard.',
 			'controllerMode',
 			'bool',
-			false);
+			true);
 		addOption(option);
 
 		//I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
