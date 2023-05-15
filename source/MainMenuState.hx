9b9b345159eeb341dc7469303061e02b357b5e02
MasterX <95196453+Default-name123@users.noreply.github.com>
2023-05-14 20:40:16 -0500

    update source/MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -132,7 +132,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 		
-    var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Ported by MarioMster & MaysLastPlay", 12);
+    var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Ported by MarioMaster & MaysLastPlay", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat(Paths.font("GhostKidAOE.otf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
